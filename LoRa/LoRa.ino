7e628663d64d8bfa660b5a3cb57a87e40187fc07
lewisxhe <lewishe@outlook.com>
2019-02-14 13:04:24 +0800

    update readme and add sdcard support

--- LoRa/LoRa.ino
+++ LoRa/LoRa.ino
@@ -2,26 +2,17 @@
 #include <LoRa.h>
 #include "ds3231.h"
 #include <WiFi.h>
+#include <SD.h>
 
 OLED_CLASS_OBJ display(OLED_ADDRESS, OLED_SDA, OLED_SCL);
 
-#define WIFI_SSID       "wifi ssid"
-#define WIFI_PASSWORD   "wifi password"
+#define WIFI_SSID       "Xiaomi"
+#define WIFI_PASSWORD   "12345678"
 
 void setup()
 {
     Serial.begin(115200);
     while (!Serial);
-
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-        Serial.println("WiFi Connect Fail");
-        esp_restart();
-    }
-    Serial.print("Connected : ");
-    Serial.println(WiFi.SSID());
-    Serial.print("IP:");
-    Serial.println(WiFi.localIP().toString());
 
     if (OLED_RST > 0) {
         pinMode(OLED_RST, OUTPUT);
@@ -39,6 +30,23 @@ void setup()
     display.setTextAlignment(TEXT_ALIGN_CENTER);
     display.drawString(display.getWidth() / 2, display.getHeight() / 2, LORA_SENDER ? "LoRa Sender" : "LoRa Receiver");
     display.display();
+    delay(2000);
+
+    if (SDCARD_CS >  0) {
+        display.clear();
+        SPIClass sdSPI(VSPI);
+        sdSPI.begin(SDCARD_SCLK, SDCARD_MISO, SDCARD_MOSI, SDCARD_CS);
+        if (!SD.begin(SDCARD_CS, sdSPI)) {
+            display.drawString(display.getWidth() / 2, display.getHeight() / 2, "SDCard  FAIL");
+        } else {
+            display.drawString(display.getWidth() / 2, display.getHeight() / 2 - 16, "SDCard  PASS");
+            uint32_t cardSize = SD.cardSize() / (1024 * 1024);
+            display.drawString(display.getWidth() / 2, display.getHeight() / 2, "Size: " + String(cardSize) + "MB");
+        }
+        display.display();
+        delay(2000);
+    }
+
 
     String info = ds3231_test();
     if (info != "") {
@@ -50,12 +58,34 @@ void setup()
         delay(2000);
     }
 
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+        display.clear();
+        Serial.println("WiFi Connect Fail");
+        display.drawString(display.getWidth() / 2, display.getHeight() / 2, "WiFi Connect Fail");
+        display.display();
+        delay(2000);
+        esp_restart();
+    }
+    Serial.print("Connected : ");
+    Serial.println(WiFi.SSID());
+    Serial.print("IP:");
+    Serial.println(WiFi.localIP().toString());
+    display.clear();
+    display.drawString(display.getWidth() / 2, display.getHeight() / 2, "IP:" + WiFi.localIP().toString());
+    display.display();
+    delay(2000);
+
     SPI.begin(CONFIG_CLK, CONFIG_MISO, CONFIG_MOSI, CONFIG_NSS);
     LoRa.setPins(CONFIG_NSS, CONFIG_RST, CONFIG_DIO0);
-    Serial.println("LoRa Receiver");
-    if (!LoRa.begin(LORA_PERIOD)) {
+    if (!LoRa.begin(BAND)) {
         Serial.println("Starting LoRa failed!");
         while (1);
+    }
+    if (!LORA_SENDER) {
+        display.clear();
+        display.drawString(display.getWidth() / 2, display.getHeight() / 2, "LoraRecv Ready");
+        display.display();
     }
 }
 
@@ -69,13 +99,13 @@ void loop()
         rssi = WiFi.RSSI();
         display.clear();
         display.setTextAlignment(TEXT_ALIGN_CENTER);
-        display.drawString(display.getWidth() / 2, display.getHeight() / 2, "Send Count:" + String(rssi));
+        display.drawString(display.getWidth() / 2, display.getHeight() / 2, "Send RSSI:" + String(rssi));
         display.display();
         LoRa.beginPacket();
         LoRa.print("WiFi RSSI: ");
         LoRa.print(rssi);
         LoRa.endPacket();
-    }else{
+    } else {
         Serial.println("WiFi Connect lost ...");
     }
     delay(2500);
--- LoRa/board_def.h
+++ LoRa/board_def.h
@@ -1,12 +1,16 @@
 #include <Arduino.h>
 
-
-
-#define LORA_V1_0_OLED  1
+#define LORA_V1_0_OLED  0
 #define LORA_V1_2_OLED  0
+#define LORA_V1_6_OLED  0
+#define LORA_V2_0_OLED  1
 
+#define LORA_SENDER 0
+// #define LORA_SENDER 1
 
-#define LORA_SENDER 1
+// #define LORA_PERIOD 868  
+// #define LORA_PERIOD 915     
+#define LORA_PERIOD 433  
 
 #if LORA_V1_0_OLED
 #include <Wire.h> 
@@ -25,8 +29,11 @@
 // !    There are two versions of TTGO LoRa V1.0, 
 // !    the 868 version uses the 3D WiFi antenna, and the 433 version uses the PCB antenna. 
 // !    You need to change the frequency according to the board.
-#define LORA_PERIOD 868E6       // ! 868 version
-// #define LORA_PERIOD 433E6       // ! 433 version
+
+#define SDCARD_MOSI -1
+#define SDCARD_MISO -1
+#define SDCARD_SCLK -1
+#define SDCARD_CS   -1
 
 #elif LORA_V1_2_OLED
 //Lora V1.2 ds3231
@@ -43,18 +50,67 @@
 #define CONFIG_NSS  18
 #define CONFIG_RST  23
 #define CONFIG_DIO0 26
-#define LORA_PERIOD 433E6
+
+#define SDCARD_MOSI -1
+#define SDCARD_MISO -1
+#define SDCARD_SCLK -1
+#define SDCARD_CS   -1
 
 #define ENABLE_DS3231
 
-#else
-//T3_V1.1
+#elif LORA_V1_6_OLED
+#include <Wire.h> 
+#include "SSD1306Wire.h" 
+#define OLED_CLASS_OBJ  SSD1306Wire
+#define OLED_ADDRESS    0x3C
+#define OLED_SDA    21
+#define OLED_SCL    22
+#define OLED_RST    -1
+
 #define CONFIG_MOSI 27
 #define CONFIG_MISO 19
 #define CONFIG_CLK  5
 #define CONFIG_NSS  18
-#define CONFIG_RST  -1
+#define CONFIG_RST  23
 #define CONFIG_DIO0 26
-#define LORA_PERIOD 433E6
 
+#define SDCARD_MOSI 15
+#define SDCARD_MISO 2
+#define SDCARD_SCLK 14
+#define SDCARD_CS   13
+
+#elif LORA_V2_0_OLED
+#include <Wire.h> 
+#include "SSD1306Wire.h" 
+#define OLED_CLASS_OBJ  SSD1306Wire
+#define OLED_ADDRESS    0x3C
+#define OLED_SDA    21
+#define OLED_SCL    22
+#define OLED_RST    -1
+
+#define CONFIG_MOSI 27
+#define CONFIG_MISO 19
+#define CONFIG_CLK  5
+#define CONFIG_NSS  18
+#define CONFIG_RST  23
+#define CONFIG_DIO0 26
+
+#define SDCARD_MOSI 15
+#define SDCARD_MISO 2
+#define SDCARD_SCLK 14
+#define SDCARD_CS   13
+
+#else
+#error "please select board"
+#endif
+
+
+#if LORA_PERIOD == 433
+#define BAND 433E6
+#elif LORA_PERIOD == 868
+#define BAND 868E6
+#elif LORA_PERIOD = 915
+#define BAND 915E6
+#else
+#error "Please select the correct lora frequency"
 #endif
